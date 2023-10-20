da4b709f10979aeba85bc8060225e6bfb962dce0
Brian Macy <brian@senzing.com>
2023-10-20 08:33:05 +0000

    Update README and some minor cleanup

--- buildit.sh
+++ buildit.sh
@@ -1,2 +1,2 @@
-go build -buildmode c-shared -o libsz_cgo_encrypt_plugin.so encrypt_dummy.go plugin_c_itf.go encrypt_itf.go
+go build -buildmode c-shared -o libsz_cgo_dummy.so encrypt_dummy.go plugin_c_itf.go encrypt_itf.go
 
